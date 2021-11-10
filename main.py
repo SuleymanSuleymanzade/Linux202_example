1924d7bce6ed2712294167fbed0e3db4d525e35e
suleyman suleymanzade <suleyman.suleymanzade.nicat@gmail.com>
2021-11-10 13:38:09 +0400

    requests added

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from utils import get_triangle_area 
-
+from bs4 import BeautifulSoup
+from requests import get
 
 def main():
     a = 3;
     h = 6;
+    req = get("https://www.day.az")
+    print(req.text)
     res = get_triangle_area(a, h) 
     print(res) 
     print('this is new added message')
